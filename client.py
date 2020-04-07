41b79a8dbf2b9b566987be603a84cdef85d517b9
최형우 <a11dsd123@gmail.com>
2020-04-07 09:34:29 +0900

    Aggro 커맨드 수정

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 import discord
 import asyncio
 import constant as Constant
-from command import Action
+from command import Action, Command
 
 class Client(discord.Client):
     action = Action()
@@ -16,9 +16,6 @@ class Client(discord.Client):
         if self.check_author_is_user(message.author):
            return
 
-        elif message.content == '애들아 나 할말 있어...':
-            send_message = self.action.match_action('애들아 나 할말 있어...')
-
         else:
             send_message = self.check_command(message)
 
@@ -31,4 +28,6 @@ class Client(discord.Client):
     # 커맨드 확인하기
     def check_command(self, message):
         if message.content.startswith(Constant.PREFIX_CHAR):
-                return self.action.match_action(message.content)
+            return self.action.match_action(message.content) 
+        elif message.content == Command.AGGRO.value:
+            return self.action.aggro()
